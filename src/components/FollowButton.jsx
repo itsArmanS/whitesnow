233d5b6120d6ddb4eb08489dd4f79d9b5f9f7b95
alien <148527832+coding-eyel@users.noreply.github.com>
2024-03-31 23:25:43 +0400

    Added FollowButton.jsx

---
+++ src/components/FollowButton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/profileTopUsers.css"
+
+function FollowButton() {
+
+  return (
+    <button className="follow-button">FOLLOW</button>
+  )
+}
+
+export default FollowButton
--- src/components/GetTopUsers.jsx
+++ src/components/GetTopUsers.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "../styles/profileTopUsers.css"
+import FollowButton from "./FollowButton";
 
 function GetTopUsers() {
   const [users, setUsers] = useState([])
@@ -24,10 +25,24 @@ function GetTopUsers() {
 
   return (
     <>
-      {users.map(user => (
+      {users.map((user, index) => (
 
         <div className="top-user-scorecard">
-
+          <div className="leaderboard-number">
+            <span>#{index + 1}</span>
+          </div>
+          <div className="user-info-wrapper">
+            <div>
+              <img src="/public/images/default-user-image" alt="" />
+              <span>@{user.username}</span>
+            </div>
+            <div>
+              Flakes: {user.profile.flakes}
+            </div>
+          </div>
+          <div className="follow-button-wrapper">
+            <FollowButton />
+          </div>
         </div>
 
       ))}
